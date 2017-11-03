1f753d16c80f00e2bc0629f4b47a1936ec880c26
xrr2016 <xiaoranran1993@163.com>
2017-11-04 01:09:10 +0800

    :zap: edit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-import sum from './sum'
+import _ from 'lodash'
+import './style.css'
+import avatar from './avatar.jpg'
 
-console.log(sum(1 + 1))
+function component () {
+  let element = document.createElement('div')
+
+  element.innerHTML = _.join(['Hello', 'Webpack!'], ' ')
+  element.classList.add('hello')
+
+  const img = new Image()
+  img.src = avatar
+
+  element.appendChild(img)
+
+  return element 
+}
+
+document.body.appendChild(component())
--- webpack.config.js
+++ webpack.config.js
@@ -3,15 +3,22 @@ const path = require('path')
 module.exports = {
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
   },
   module: {
     rules: [
       {
-        test: /\.js$/,
-        use: 'babel-loader',
-        exclude: /node_modules/
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ['file-loader']
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ['file-loader']
       }
     ]
   }
